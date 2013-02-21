c508832fd54757d2b2cc336a0f9c41e27e7f4a91
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-20 19:05:29 -0500

    Removing whitespace

--- command_runner.rb
+++ command_runner.rb
@@ -6,16 +6,16 @@ require 'rubygems'
 require 'highline'
 
 # I'm going to be running lots of commands, and explaining what is going on,
-# so I created a simple class to handle this. 
+# so I created a simple class to handle this.
 # I could instead create a function.
 class CommandRunner
-  
+
   def self.context(&block)
     CommandRunner.new.tap { |cr|
       cr.instance_eval(&block)
     }
   end
-  
+
   VALUE = "2"
 
   def run(command, message)
@@ -23,7 +23,7 @@ class CommandRunner
     Kernel.system("cd #{target_directory} && #{command}")
     ask("[Press Enter to continue]")
   end
-  
+
   def info(*args)
     message = messagify(true, *args)
     highline.say(highline.color(message, :white, :on_blue))
@@ -33,12 +33,12 @@ class CommandRunner
     message = messagify(false, *args)
     highline.ask(highline.color(message, :blue), &block)
   end
-  
+
   def target_directory
     return @target_directory if @target_directory
-    
+
     response = File.join(ENV['HOME'],'/Desktop/demo') # highline.ask('Where do you want the demo?')
-    
+
     # Creating an instance variable which is accessible within the scope
     # of this newly object.
     @target_directory = Pathname.new(response)
@@ -51,22 +51,22 @@ class CommandRunner
     end
     @target_directory
   end
-  
+
   protected
   def messagify(padding = false, *args )
     length = 80
 
     lines = args.flatten.join("\n").gsub(/(.{1,#{length}})( +|$)\n?|(.{#{length}})/,
         "\\1\\3\n").split(/\n/)
-        
+
     empty_line = "\n" << " " * (6 + length)
     lines.inject("#{empty_line if padding}") {|m,v|
       m << "\n"  << " " * 3 << v << " " * (3 + length - v.length )
     }.tap {|response|
       response << empty_line << "\n"  << "   (see below)" <<  " " * 72 << empty_line if padding
-    } 
+    }
   end
-  
+
   def highline
     @highline ||= HighLine.new
   end
